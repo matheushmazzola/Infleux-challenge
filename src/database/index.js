a261a462325f31c5728170270090aa8363a73aa2
matheus mazzola <matheushmazzola@gmail.com>
2022-11-16 23:42:48 -0300

    url mongodb

--- src/database/index.js
+++ src/database/index.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect(' mongodb+srv://krypt:gYWbT5NN9nAGWsHK@cluster0.9tpbx9m.mongodb.net/node'), { useMongoClient: true };
+mongoose.connect('mongodb+srv://krypt:<gYWbT5NN9nAGWsHK>@cluster0.9tpbx9m.mongodb.net/node'), { useMongoClient: true };
 
 mongoose.Promise = global.Promise;
 
