7c67757a62d02ec0b98ac1aef54e535a4b2229ce
matheus mazzola <matheushmazzola@gmail.com>
2022-11-18 15:36:41 -0300

    mensagem

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ app.use(cors());
 app.use(express.static(path));
 
 app.get('/', (request, response)=>{
+    return response.send("https://github.com/matheushmazzola/Infleux-challenge-backend");
 })
 
 app.listen(process.env.PORT || 3001, (error)=>{
